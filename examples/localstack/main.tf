5c413e4bc26e7600fa7f45d61b9c0b3245ce2a37
Mohammed Yahya <my.almusaddar@gmail.com>
2021-04-11 08:47:56 +0300

    📦 NEW: add localstack

---
+++ examples/localstack/main.tf
@@ -0,0 +1,42 @@
+module "test" {
+  source = "../../"
+
+  kms_key_id        = null   # kms_key_id - (optional) is a type of string
+  name              = "test" # name - is a type of string
+  retention_in_days = null   # retention_in_days - (optional) is a type of number
+  tags              = {}     # tags - (optional) is a type of map of string
+
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
